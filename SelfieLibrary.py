c88a6841bbb472d198f060b4268ea5c2d4622ec0
madsq98-desktop <mads.q@outlook.com>
2022-07-15 20:55:16 +0200

    Update SelfieLibrary.py

--- SelfieLibrary.py
+++ SelfieLibrary.py
@@ -4,45 +4,26 @@ import selfieOptions as opt
 from datetime import datetime
 import pygame
 import io
+import pygame.camera
 
 def pyGameTest():
     pygame.init()
-    screen = pygame.display.set_mode((0, 0))
+    pygame.camera.init()
 
-    # Init camera
-    camera = PiCamera()
-    camera.resolution = (1280, 720)
-    camera.crop = (0.0, 0.0, 1.0, 1.0)
+    screen = pygame.display.set_mode((640, 480), 0)
+    cam_list = pygame.camera.list_cameras()
+    cam = pygame.camera.Camera(cam_list[0], (32, 24))
+    cam.start()
 
-    x = (screen.get_width() - camera.resolution[0]) / 2
-    y = (screen.get_height() - camera.resolution[1]) / 2
-
-    # Init buffer
-    rgb = bytearray(camera.resolution[0] * camera.resolution[1] * 3)
-
-    # Main loop
-    exitFlag = True
-    while (exitFlag):
-        for event in pygame.event.get():
-            if (event.type is pygame.MOUSEBUTTONDOWN or
-                    event.type is pygame.QUIT):
-                exitFlag = False
-
-        stream = io.BytesIO()
-        camera.capture(stream, use_video_port=True, format='rgb')
-        stream.seek(0)
-        stream.readinto(rgb)
-        stream.close()
-        img = pygame.image.frombuffer(rgb[0:
-                                          (camera.resolution[0] * camera.resolution[1] * 3)],
-                                      camera.resolution, 'RGB')
-
-        screen.fill(0)
-        if img:
-            screen.blit(img, (x, y))
-
+    while True:
+        image1 = cam.get_image()
+        image1 = pygame.transform.scale(image1, (640, 480))
+        screen.blit(image1, (0, 0))
         pygame.display.update()
 
-    camera.close()
-    pygame.display.quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                cam.stop()
+            pygame.quit()
+            sys.exit()
 
