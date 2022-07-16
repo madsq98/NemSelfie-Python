69c660499384240f72326524e3e43c31b22dd0b4
madsq98-desktop <mads.q@outlook.com>
2022-07-16 13:35:11 +0200

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,5 +1,8 @@
 import git
+import os
 
+os.environ['GIT_USERNAME'] = "madsq98"
+os.environ['GIT_PASSWORD'] = "yuyatrA48a"
 repo = git.Repo('/home/pi/NEMSELFIE/nemselfie-python')
 repo.remotes.origin.pull()
 
