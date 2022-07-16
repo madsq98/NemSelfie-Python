d1b872e02cdba5fc4e82c0d76d11f8f5cae4408e
madsq98-desktop <mads.q@outlook.com>
2022-07-16 13:47:03 +0200

    Update SelfieLibrary.py

--- SelfieLibrary.py
+++ SelfieLibrary.py
@@ -10,22 +10,28 @@ def pyGameTest():
     pygame.init()
     pygame.camera.init()
 
-    pygame.mouse.set_visible(False)
+    #pygame.mouse.set_visible(False)
 
     screen = pygame.display.set_mode((800, 480), 0)
     cam_list = pygame.camera.list_cameras()
     cam = pygame.camera.Camera(cam_list[0], (800, 480))
     cam.start()
 
+    takePicture = False
     while True:
         image1 = cam.get_image()
         image1 = pygame.transform.scale(image1, (800, 480))
-        screen.blit(image1, (0, 0))
+        if takePicture:
+            screen.blit(image1, (0, 0))
+        else:
+            screen.fill((0, 0, 0))
+
         pygame.display.update()
 
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 cam.set_controls(False, False, 100)
+                takePicture = True
 
             if event.type == pygame.QUIT:
                 cam.stop()
