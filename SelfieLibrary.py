ba45c8c10ee29c6cea9f2c7094d984a66547a30a
madsq98-desktop <mads.q@outlook.com>
2022-07-16 15:58:11 +0200

    Update SelfieLibrary.py

--- SelfieLibrary.py
+++ SelfieLibrary.py
@@ -23,23 +23,26 @@ def pyGameTest():
     while True:
         image1 = cam.get_image()
         image1 = pygame.transform.scale(image1, (opt.SCREEN_X, opt.SCREEN_Y))
+
+        screen.blit(image1, (0, 0))
+
         if not takePicture:
-            screen.blit(image1, (0, 0))
+            font = pygame.font.Font(None, 50)
+            text = font.render("Tryk her for at tage et NemSelfie! :-)", True, (255, 255, 255))
+            text_rect = text.get_rect(center=(opt.SCREEN_X / 2, opt.SCREEN_Y / 2))
+            screen.blit(text, text_rect)
         else:
-            screen.fill((0, 0, 0))
-
-        font = pygame.font.Font(None, 50)
-        text = font.render("Tryk her for at tage et NemSelfie! :-)", True, (255, 255, 255))
-        text_rect = text.get_rect(center=(opt.SCREEN_X / 2, opt.SCREEN_Y / 2))
-
-        screen.blit(text, text_rect)
-
+            font = pygame.font.Font(None, 50)
+            text = font.render("Er du klar?", True, (255, 255, 255))
+            text_rect = text.get_rect(center=(opt.SCREEN_X / 2, opt.SCREEN_Y / 2))
+            screen.blit(text, text_rect)
+            
         pygame.display.update()
 
         for event in pygame.event.get():
             if event.type == pygame.FINGERDOWN:
-                cam.set_controls(False, False, 100)
-                #takePicture = True
+                cam.set_controls(False, False, 50)
+                takePicture = True
 
             if event.type == pygame.QUIT:
                 cam.stop()
