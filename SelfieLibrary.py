231ace9907f8dc38b69d354143e376ec9e49e5bd
madsq98-desktop <mads.q@outlook.com>
2022-07-16 16:23:47 +0200

    Update SelfieLibrary.py

--- SelfieLibrary.py
+++ SelfieLibrary.py
@@ -6,6 +6,7 @@ import pygame
 import io
 import pygame.camera
 
+
 def pyGameTest():
     pygame.init()
     pygame.camera.init()
@@ -20,8 +21,12 @@ def pyGameTest():
     cam.set_controls(False, False, opt.IDDLE_BRIGHTNESS)
 
     takePicture = False
-    timerSet = False
-    counter = opt.IDDLE_BRIGHTNESS
+    brightnessTimerSet = False
+    brightnessCounter = opt.IDDLE_BRIGHTNESS
+    brightnessTimerDone = False
+
+    pictureCountdownTimerSet = False
+    pictureCountdownCounter = 0
 
     while True:
         image1 = cam.get_image()
@@ -35,23 +40,45 @@ def pyGameTest():
             text_rect = text.get_rect(center=(opt.SCREEN_X / 2, opt.SCREEN_Y / 2))
             screen.blit(text, text_rect)
         else:
+            if not pictureCountdownTimerSet & brightnessTimerDone:
+                pygame.time.set_timer(pygame.USEREVENT, 1000)
+
             font = pygame.font.Font(None, 50)
-            text = font.render(opt.PICTURE_TEXT_BEFORE, True, (255, 255, 255))
+
+            if pictureCountdownCounter == 0:
+                textToRender = opt.PICTURE_TEXT_BEFORE
+            elif pictureCountdownCounter == 1:
+                textToRender = opt.PICTURE_TEXT_1
+            elif pictureCountdownCounter == 2:
+                textToRender = opt.PICTURE_TEXT_2
+            elif pictureCountdownCounter == 3:
+                textToRender = opt.PICTURE_TEXT_3
+            else:
+                textToRender = ""
+
+            text = font.render(textToRender, True, (255, 255, 255))
             text_rect = text.get_rect(center=(opt.SCREEN_X / 2, opt.SCREEN_Y / 2))
             screen.blit(text, text_rect)
 
-        if not timerSet & takePicture:
+        if not brightnessTimerSet & takePicture:
             pygame.time.set_timer(pygame.USEREVENT, 50)
-            timerSet = True
+            brightnessTimerSet = True
 
         pygame.display.update()
 
         for event in pygame.event.get():
+            # Functionality for fading from Iddle Brightness to Picture Brightness
             if event.type == pygame.USEREVENT:
-                if counter < opt.PICTURE_BRIGHTNESS:
-                    counter += 1
-                    cam.set_controls(False, False, counter)
+                if brightnessCounter < opt.PICTURE_BRIGHTNESS & brightnessTimerDone == False:
+                    brightnessCounter += 1
+                    cam.set_controls(False, False, brightnessCounter)
+                else:
+                    brightnessTimerDone = True
 
+                if pictureCountdownCounter < 3 & brightnessTimerDone:
+                    pictureCountdownCounter += 1
+
+            # Detect touch screen input
             if event.type == pygame.FINGERDOWN:
                 takePicture = True
 
